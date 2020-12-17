3530bc0d9a6ea3333db49a9f3da02754dad7718a
GiedriusGG <76166602+GiedriusGG@users.noreply.github.com>
2020-12-17 15:31:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace school_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1: Write a C# program to create a new string from a given string (length 1 or more ) with the first character added at the front and back.");
+            Console.WriteLine("Please enter Test Data:");
+            var EnteredText = Console.ReadLine();
+            if (EnteredText.Length > 0)
+            {
+                Console.WriteLine("Output: " + EnteredText[0] + EnteredText + EnteredText[0]);
+            }
+            else
+            {
+                Console.WriteLine("Entered text is less than 1 symbol!");
+            }
+            
+        }
+    }
+}
